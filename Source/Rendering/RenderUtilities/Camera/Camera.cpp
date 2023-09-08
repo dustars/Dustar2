17ac36862afc5e31ff8a7d32aafcca3eadccfb54
Hanning Zhao <auty789@hotmail.com>
2023-09-09 02:18:55 +0800

    1

--- Source/Rendering/RenderUtilities/Camera/Camera.cpp
+++ Source/Rendering/RenderUtilities/Camera/Camera.cpp
@@ -49,7 +49,7 @@ mat4 Camera::BuildViewMatrix()
 {
 	return	mat4::Rotation(-pitch, Vector3(1, 0, 0)) *
 		mat4::Rotation(-yaw, Vector3(0, 1, 0)) *
-		mat4::Translation(-position) ;
+		mat4::Translation(-position);
 }
 
 void Camera::CameraControlUpdate(float msec)
